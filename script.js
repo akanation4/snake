85d38da60647ab55c37daa1ddc947cfc65587dd8
akanation4 <103402576+akanation4@users.noreply.github.com>
2023-01-20 22:01:19 +0900

    Add snake face

--- script.js
+++ script.js
@@ -1,22 +1,45 @@
 new Vue({
     el: '#app',
 
+    // Game data
     data: {
+        stage_side_length: 10,
 
+        snake: {
+
+            // Sanake head position(initial value)
+            head_pos: {
+                x: 1,
+                y: 3,
+            },
+        },
     },
 
+    // Game initialization
     created() {
 
     },
 
+    // Action triggered by changes in data/computed
     watch: {
 
     },
 
+    // Status based on data
     computed: {
 
+        // Number of cells in the stage(=100)
+        stage_cells_count() {
+            return this.stage_side_length * this.stage_side_length
+        },
+
+        // Index of the snake head cell
+        snake_head_index() {
+            return this.snake.head_pos.y * this.stage_side_length + this.snake.head_pos.x
+        }
     },
 
+    // Methods to change data
     methods: {
 
     },
